79ea59d9ff97dc52178d3e36ca1b97433b1cfff7
MazenAtlam <mazenatef5510@gmail.com>
2024-07-05 23:51:30 +0300

    Add tests to test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,6 +10,20 @@ class TestBaseModel(unittest.TestCase):
 
     model = BaseModel()
 
+    def test___str__(self):
+        """A test for the print instance format string"""
+
+        self.assertRegex(TestBaseModel.model.__str__(),
+        "/^\[\w+\]\s\(([a-f0-9]{4,12}\-{0,1}){5}\)\s\{(?:\'\S+\'\:\s*\S+)*\}$")
+
+    def test_to_dict(self):
+        """A test for the method to_dict()"""
+
+        dict_copy = TestBaseModel.model.__dict__.copy()
+        dict_copy.update({'__class__': "BaseModel"})
+        self.assertIn({'__class__': "BaseModel"}, TestBaseModel.model.to_dict())
+        self.assertDictEqual(dict_copy, TestBaseModel.model.to_dict())
+
     def test_save(self):
         """A test for the method save()"""
 
